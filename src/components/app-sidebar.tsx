0f57619825815ebe9e07dce2a6f981383d70ddaa
Winme <winme0308@gmail.com>
2025-03-21 17:54:49 +0800

    sidebar

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,34 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import { NavGroup } from "./nav-group";
+import { NavUser } from "./nav-user";
+import { TeamSwitcher } from "./team-switcher";
+import { sidebarData, treeData } from "./sidebar-data";
+import { TreeView } from "./ui/tree-view";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible='icon' variant='floating' {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={sidebarData.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        {sidebarData.navGroups.map((props) => (
+          <NavGroup key={props.title} {...props} />
+        ))}
+      </SidebarContent>
+      <SidebarContent>
+        <TreeView data={treeData} expandAll />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={sidebarData.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
